93e1af9bbeefee44e643f37d3e9110bc8c6418e8
Ahmad Atef <ahmad.atef@scout24.com>
2017-11-01 20:49:45 +0100

    Added View model

---
+++ Walt Disney/View Models/MoviesListViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  MoviesListViewModel.swift
+//  Walt Disney
+//
+//  Created by Ahmad Atef on 11/1/17.
+//  Copyright © 2017 Ahmad Atef. All rights reserved.
+//
+
+import Foundation
+
+protocol MoviesList {
+    func listMovies (onSuccess: ([Movie]) -> (), onFail: (String) -> ())
+    func numberOfMovies() -> Int
+    func movieAtIndex(index: Int) -> Movie
+}
+
+class MoviesListDataModel : MoviesList{
+
+    func sayHello(name: String) -> String {
+        return "Hello \(name)"
+    }
+
+    private var movies : [Movie] = [Movie(name: "X", imageCover: "", genres: [.Carton,.Drama], rating: 4.5, story: "asdasd", releaseDate: "123123"),
+                                    Movie(name: "Y", imageCover: "", genres: [.Carton,.Drama], rating: 4.5, story: "asdasd", releaseDate: "123123"),
+                                    Movie(name: "Z", imageCover: "", genres: [.Carton,.Drama], rating: 4.5, story: "asdasd", releaseDate: "123123")]
+
+    func listMovies (onSuccess: ([Movie]) -> (), onFail: (String) -> ()){
+        onSuccess(movies)
+    }
+
+    func numberOfMovies() -> Int{
+        return movies.count
+    }
+
+    func movieAtIndex(index: Int) -> Movie{
+        return movies[index]
+    }
+}
