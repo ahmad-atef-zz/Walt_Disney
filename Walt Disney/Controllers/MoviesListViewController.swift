6ab977d20b58821400781a28c74b3d6e8c72b2c9
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2018-04-20 05:41:30 +0200

    Added MoviesListViewController.

--- Walt Disney/Controllers/MoviesListViewController.swift
+++ Walt Disney/Controllers/MoviesListViewController.swift
@@ -10,42 +10,25 @@ import UIKit
 
 class MoviesListViewController: UIViewController {
 
-
-    let movieView : MovieCell = {
-        let view = MovieCell()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .red
-        return view
-    }()
-
     //MARK: - Proprties -
-    let dataModel : MoviesList = MoviesListDataModel()
+    
 
     //MARK: - View Life Cycle -
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
-        applyContstraint()
-    }
-
-    func setupView(){
-        self.view.addSubview(movieView)
-    }
-    func applyContstraint() {
-        movieView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        movieView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        movieView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        movieView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        movieView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        listMovies()
     }
 
     //MARK: - Functions -
     func listMovies() {
-        dataModel.listMovies(onSuccess: { (movies) in
-            print(movies.count)
-        }) { (error) in
-            print(error)
-        }
+        
+//        let movieJson = "Movie.json".contentOfFile()
+//        let movie = try! Movie(movieJson)
+//        print(movie.title)
+//        if let jsonString = try! movie.jsonString(){
+//            print(jsonString)
+//        }
     }
 }
+
 
