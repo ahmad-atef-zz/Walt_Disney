d3feb27a3230f97f057cd57a3a02f6d6492f38a9
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2018-04-20 05:38:49 +0200

    Added UIExtentions file.

--- Walt Disney/Extensions/UIExtentions.swift
+++ Walt Disney/Extensions/UIExtentions.swift
@@ -20,30 +20,3 @@ extension UIImageView{
         translatesAutoresizingMaskIntoConstraints = false
     }
 }
-
-
-//        let topAnchor = testingView.topAnchor.constraint(equalTo: self.view.topAnchor)
-//        let bottomAnchor = testingView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
-//        let rightAnchor = testingView.rightAnchor.constraint(equalTo: self.view.rightAnchor)
-//        let leftAnchor = testingView.leftAnchor.constraint(equalTo: self.view.leftAnchor)
-//
-//        let allConstraints : [NSLayoutConstraint] = [topAnchor,leftAnchor,bottomAnchor,rightAnchor]
-//        NSLayoutConstraint.activate(allConstraints)
-
-
-//        let centerXConstraint = testingView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
-//        let centerYConstraint = testingView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor )
-//        let widthConstrint = testingView.widthAnchor.constraint(equalToConstant: 150.0)
-//        let heightConstrint = testingView.heightAnchor.constraint(equalToConstant: 150.0)
-//
-//        let allConstrints = [centerXConstraint, centerYConstraint,widthConstrint,heightConstrint]
-//        NSLayoutConstraint.activate(allConstrints)
-
-//
-//        let widthConstrint = testingView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.6)
-//        let heightConstrint = testingView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.75)
-//        let trailingConstrint = testingView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
-//        let bottomConstrint = testingView.bottomAnchor.constraint(equalTo: self.bottomLayoutGuide.topAnchor)
-//        let allConstrints = [trailingConstrint, bottomConstrint,widthConstrint,heightConstrint]
-//
-//        NSLayoutConstraint.activate(allConstrints)
