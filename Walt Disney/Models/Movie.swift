826e9102da1df82e21294fcbfbb782e6b6145280
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2018-06-26 04:44:08 +0200

    Removed unused Movies struct.

--- Walt Disney/Models/Movie.swift
+++ Walt Disney/Models/Movie.swift
@@ -65,35 +65,3 @@ extension Movie {
 }
 
 
-
-
-
-struct Movies: Codable {
-    let movies: [Movie]
-}
-
-extension Movies {
-    init(data: Data) throws {
-        self = try JSONDecoder().decode(Movies.self, from: data)
-    }
-    
-    init(_ json: String, using encoding: String.Encoding = .utf8) throws {
-        guard let data = json.data(using: encoding) else {
-            throw NSError(domain: "JSONDecoding", code: 0, userInfo: nil)
-        }
-        try self.init(data: data)
-    }
-    
-    init(fromURL url: URL) throws {
-        try self.init(data: try Data(contentsOf: url))
-    }
-    
-    func jsonData() throws -> Data {
-        return try JSONEncoder().encode(self)
-    }
-    
-    func jsonString(encoding: String.Encoding = .utf8) throws -> String? {
-        return String(data: try self.jsonData(), encoding: encoding)
-    }
-}
-
