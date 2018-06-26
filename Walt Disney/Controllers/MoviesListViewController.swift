b95bdcd4b55ebed9a2137ca965c2e39b6e3d337a
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2018-06-26 04:47:06 +0200

    Clean up MovieListViewController.

--- Walt Disney/Controllers/MoviesListViewController.swift
+++ Walt Disney/Controllers/MoviesListViewController.swift
@@ -12,39 +12,43 @@ import SwifterSwift
 class MoviesListViewController: UIViewController {
 
     //MARK: - Proprties -
-    private let allViews: [UIView] = [UIView(frame: .zero),UIView(frame: .zero),UIView(frame: .zero)]
     private let movieCell = MovieCell()
     private let movie = try! Movie("Movie.json".contentOfFile())
+
+    private let warningCell = WarningCell()
+    let advertisement = Advertisement(url: URL(string: "www.apple.com")!)
 
     //MARK: - View Life Cycle -
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setupThreeViews()
-//        setupThreeViewsConstraints()
+        
         setupMovieCellView()
+        setupAdvertisemtnsCell()
         setupMovieCellConstraints()
+        setupAdsCellConstraints()
 
-    }
-    
-    func print() {
-        print("I'm back 🎉🤘🚀")
-    }
 
-    private func setupThreeViews() {
-        for view in allViews {
-            view.backgroundColor = .random
-            self.view.addSubview(view)
-        }
-    }
-    
-    private func setupThreeViewsConstraints() {
-        setupThreeEquallySideBySideView()
     }
 
     private func setupMovieCellView() {
         movieCell.backgroundColor = .random
         view.addSubview(movieCell)
         movieCell.configure(data: movie)
+    }
+    private func setupAdvertisemtnsCell() {
+        warningCell.backgroundColor = .random
+        view.addSubview(warningCell)
+        warningCell.configure(data: "Fuck You 🖕")
+    }
+
+    private func setupAdsCellConstraints() {
+        let margins = self.view.layoutMarginsGuide
+        warningCell.translatesAutoresizingMaskIntoConstraints = false
+
+        warningCell.topAnchor.constraint(equalTo: movieCell.bottomAnchor).isActive = true
+        warningCell.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        warningCell.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        warningCell.heightAnchor.constraint(equalToConstant: 100.0).isActive = true
     }
 
     private func setupMovieCellConstraints() {
@@ -55,36 +59,6 @@ class MoviesListViewController: UIViewController {
         movieCell.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
         movieCell.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
         movieCell.heightAnchor.constraint(equalToConstant: 100.0).isActive = true
-    }
-    
-    private func setupThreeEquallySideBySideView() {
-        let firstView = allViews[0]
-        let secondView = allViews[1]
-        let thirdView = allViews[2]
-        let margins = self.view.layoutMarginsGuide  // |----|
-
-        firstView.translatesAutoresizingMaskIntoConstraints = false
-        secondView.translatesAutoresizingMaskIntoConstraints = false
-        thirdView.translatesAutoresizingMaskIntoConstraints = false
-
-        firstView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8).isActive = true
-        secondView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8).isActive = true
-        thirdView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8).isActive = true
-        
-        firstView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -8).isActive = true
-        secondView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -8).isActive = true
-        thirdView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -8).isActive = true
-        
-        firstView.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
-        firstView.trailingAnchor.constraint(equalTo: secondView.leadingAnchor).isActive = true
-        secondView.trailingAnchor.constraint(equalTo: thirdView.leadingAnchor).isActive = true
-        thirdView.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
-        
-        firstView.widthAnchor.constraint(equalTo: secondView.widthAnchor, multiplier: 1.0).isActive = true
-        thirdView.widthAnchor.constraint(equalTo: secondView.widthAnchor, multiplier: 1.0).isActive = true
-        
-        firstView.heightAnchor.constraint(equalTo: secondView.heightAnchor, multiplier: 2.0).isActive = true
-        thirdView.heightAnchor.constraint(equalTo: secondView.heightAnchor, multiplier: 0.5).isActive = true
     }
 }
 
